9d73ba9c7b383192f483f4186e2d490daeae59e7
jamtibaes <jamtibaes@yahoo.com.br>
2022-05-27 20:23:41 -0300

    Initial commit

---
+++ src/main/java/com/letscode/ecommerce/vendaapi/EcommerceVendaApiApplication.java
@@ -0,0 +1,13 @@
+package com.letscode.ecommerce.vendaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceVendaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceVendaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/letscode/ecommerce/vendaapi/EcommerceVendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letscode.ecommerce.vendaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceVendaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
